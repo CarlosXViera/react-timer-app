41ef52161176e7a6fee149709d7e2e357182eace
CarlosXViera <CarlosXviera@gmail.com>
2017-01-24 14:14:08 -0500

    Initial Commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link, IndexLink} from 'react-router';
+
+export default class Nav extends React.Component {
+  render(){
+    return(
+      <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer Application</li>
+          <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Timer</IndexLink></li>
+          <li><Link to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Countdown</Link></li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+        <li className="menu-text">Created by <a href="http://www.github.com/carlosxviera" target="blank">Carlos X Viera</a></li>
+        </ul>
+      </div>
+      </div>
+    );
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'script!jquery/dist/jquery.min.js',
+		'script!foundation-sites/dist/js/foundation.min.js',
+		'./src/app.jsx'
+	],
+	externals: {
+		jquery: 'JQuery'
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			'window.$': "jquery",
+			'window.jQuery': "jquery"
+		})
+	],
+	output: {
+		path: __dirname,
+		filename: './public/bundle.js'
+	},
+	resolve: {
+		root: __dirname,
+		alias: {
+			Main: 'src/components/Main.jsx',
+			Nav:'src/components/Nav.jsx',
+			applicationStyles: 'src/styles/app.scss'
+		},
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: [{
+			loader: 'babel-loader',
+			query: {
+				presets: ['react', 'env', 'stage-0']
+			},
+			test: /\.jsx?$/,
+			exclude: /(node_modules|bower_components)/
+		}]
+	},
+	devtool: 'cheap-module-eval-source-map'
+};
